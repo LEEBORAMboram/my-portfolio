3bb5418b205f5748271c2cddb0d765871478393a
LEEBORAMboram <boram5528@naver.com>
2021-08-18 21:20:38 +0900

    새로운 프로젝트 생성

---
+++ MyPortfolio-1/src/main/java/com/CarAccident/MyPortfolio1Application.java
@@ -0,0 +1,13 @@
+package com.CarAccident;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPortfolio1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPortfolio1Application.class, args);
+	}
+
+}
